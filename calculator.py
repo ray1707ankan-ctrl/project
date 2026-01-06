ce8134748fd12070a90c584ffb531ed02f538583
Ankan ray <ray1707ankan@gmail.com>
2026-01-06 11:51:59 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -14,6 +14,4 @@ print(f"division {division}")
 print(f"remender k{remender}")
 
 
-name=input("Enter your first name :")
-last_name=input("Enter your last name :")
-print(f"hello {name} {last_name}! welcome to the python program.")
+
