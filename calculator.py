926a3c5d90396d65cbbc7c5c2c959c338e534890
Ankan Ray <ray1707ankan@gmail.com>
2026-01-06 11:44:57 +0530

    update README file

---
+++ calculator.py
@@ -0,0 +1,19 @@
+x=float(input("enter first num: "))
+y=float(input("enter second num: "))
+
+sum=x+y
+substraction=x-y
+multiplication=x*y
+division=x/y
+remender=x%y
+
+print(f"addition {sum}")
+print(f"substraction {substraction}")
+print(f"multiplication {multiplication}")
+print(f"division {division}")
+print(f"remender k{remender}")
+
+
+name=input("Enter your first name :")
+last_name=input("Enter your last name :")
+print(f"hello {name} {last_name}! welcome to the python program.")
---
+++ index.py
@@ -0,0 +1,3 @@
+user=input("Enter your first name :")
+user1=input("Enter your last name :")
+print(f"hello {user} {user1}!welcome to the python program.")
