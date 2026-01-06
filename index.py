64d9490d826673e2684a954a273b518c6098f519
Ankan ray <ray1707ankan@gmail.com>
2026-01-06 11:52:36 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,3 @@
-user=input("Enter your first name :")
-user1=input("Enter your last name :")
-print(f"hello {user} {user1}!welcome to the python program.")
+name=input("Enter your first name :")
+last_name=input("Enter your last name :")
+print(f"hello {name} {last_name}! welcome to the python program.")
